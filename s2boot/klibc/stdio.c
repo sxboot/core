d03faba137106c8f122faab350ed15c6630e6ff2
user94729 <user94729@omegazero.org>
2021-03-28 15:48:38 +0200

    stdio: Removed duplicate string formatting code

--- s2boot/klibc/stdio.c
+++ s2boot/klibc/stdio.c
@@ -36,71 +36,29 @@ int printf(const char* format, ...){
 }
 
 int vprintf(const char* format, va_list parameters){
+	va_list parameters2;
+	va_copy(parameters2, parameters);
 	int written = 0;
-	uint8_t color = 0x7;
-
-	while(*format != 0){
-		if(*format == '%'){
-			format++;
-			char specifier = *format;
-			if(specifier == 's'){
-				char* str = va_arg(parameters, char*);
-				uint64_t length = strlen(str);
-				print(str, color);
-				format++;
-				written += length;
-			}else if(specifier == 'u'){
-				size_t num = va_arg(parameters, size_t);
-				char* numstr = getDec(num);
-				uint64_t length = strlen(numstr);
-				print(numstr, color);
-				format++;
-				written += length;
-			}else if(specifier == 'X'){
-				size_t num = va_arg(parameters, size_t);
-				char* numstr = getHex(num) + 2; // getHex returns a string of format 0xHHHHHHHH but printf needs ...H
-				while(*numstr == '0')
-					numstr++;
-				if(*numstr == 0) // all numbers are 0, so go back one to print a single '0'
-					numstr--;
-				uint64_t length = strlen(numstr);
-				print(numstr, color);
-				format++;
-				written += length;
-			}else if(specifier == 'Y'){
-				size_t num = va_arg(parameters, size_t);
-				char* numstr = getHex(num);
-				uint64_t length = strlen(numstr);
-				print(numstr, color);
-				format++;
-				written += length;
-			}else if(specifier == 'c'){
-				char c = (char) va_arg(parameters, int);
-				printChar(c, color);
-				format++;
-				written++;
-			}else if(specifier == '%'){
-				printChar('%', color);
-				format++;
-				written++;
-			}else{
-				printChar('%', color);
-				written++;
-			}
-		}else if(*format == '&'){
-			format++;
-			char colorH = *format;
-			if(colorH > 47 && colorH < 58)
-				color = colorH - 48;
-			else if(colorH > 96 && colorH < 103)
-				color = colorH - 87;
-			format++;
-		}else{
-			printChar(*format, color);
-			format++;
-			written++;
+	int size = vsnprintf(NULL, 0, format, parameters) + 2;
+	if(size > 0){
+		char buf[size];
+		written = vsnprintf(buf, size, format, parameters2);
+		uint8_t color = 0x7;
+		for(size_t i = 0; i < size; i++){
+			if(!buf[i])
+				break;
+			else if(buf[i] == '&'){
+				char colorH = buf[i + 1];
+				if(colorH > 47 && colorH < 58)
+					color = colorH - 48;
+				else if(colorH > 96 && colorH < 103)
+					color = colorH - 87;
+				i++;
+			}else
+				printChar(buf[i], color);
 		}
 	}
+	va_end(parameters2);
 	return written;
 }
 
